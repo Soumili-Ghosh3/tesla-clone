d5d86858379d2ad16397968a47e0e9422c75dea9
Soumili-Ghosh3 <1930105@kiit.ac.in>
2021-05-15 14:01:36 +0530

    Added images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import Section from './Section'
 
 function Home() {
     return (
         <Container>
-            Home
+            <Section />
         </Container>
     )
 }
@@ -13,5 +14,5 @@ export default Home
 
 
 const Container = styled.div`
-
+    height: 100vh;
 `
