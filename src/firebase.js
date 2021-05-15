495f7b42ee9677c98367bd69410c0fb062f0beca
Soumili-Ghosh3 <1930105@kiit.ac.in>
2021-05-15 23:36:40 +0530

    Final changes

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyBCHuFq2vQA83S6zM-hs-Tzk_ljvSUX0yo",
+    authDomain: "tesla-clone-75b31.firebaseapp.com",
+    projectId: "tesla-clone-75b31",
+    storageBucket: "tesla-clone-75b31.appspot.com",
+    messagingSenderId: "590079086722",
+    appId: "1:590079086722:web:2d739db403fd07d524a49b",
+    measurementId: "G-7L6XRV6LR0"
+  });
